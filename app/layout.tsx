77b6271caaa3e6503a33adc61cfa3abbdc9b7f7b
kunalkeshan <kunalkeshan12@gmail.com>
2025-02-03 13:49:36 +0530

    feat: add Google Analytics integration and update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { DEFAULT_METADATA } from '@/config/site';
 import type { Metadata } from 'next';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-T9M92SXSYT" />
     </html>
   );
 }
